6ed924358160036a5fdabee19f4b79c681636a0a
Vikram221999 <vikram221999@gmail.com>
2022-12-16 22:44:38 +0530

    nit

--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { CarService } from '../car.service';
+import { user } from '../entity/user';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-my-profile',
@@ -7,24 +10,40 @@ import { CarService } from '../car.service';
   styleUrls: ['./my-profile.component.css']
 })
 export class MyProfileComponent implements OnInit {
-  carsDetails:any = null;
-  constructor(private carService: CarService) { 
-     this.getcarsDetails();}
+
+user!:user;
+email:string=this.cookie.get('email');
+  
+  userDetail:any = null;
+  constructor(private userService: UsersService,
+    private cookie : CookieService) { 
+    }
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    var savebutton :any;
+    var readonly :any;
+    var inputs:any;
+     savebutton = document.getElementById('savebutton');
+ readonly = true;
+ inputs = document.querySelectorAll('input[type="text"]');
+savebutton.addEventListener('click',function(){
+    
+     for (var i=0; i<inputs.length; i++) {
+     inputs[i].toggleAttribute('readonly');
+     };
+
+    if (savebutton.innerHTML == "edit") {
+      savebutton.innerHTML = "save";
+         } else {
+      savebutton.innerHTML = "edit";
+      } 
+});
+this.userdetail();
   }
 
-
-  getcarsDetails(){
-    this.carService.getcar().subscribe(
-      (resp)=>{
-        console.log(resp);
-       this.carsDetails=resp;
-        
-      },
-      (err)=>{
-        console.log(err);
-      }
-    );
+  userdetail() {
+    this.userService.getUserDetails(this.email).subscribe(data =>{
+      this.user = data;
+      console.log(this.user);
+    })
   }
-} 
+}
